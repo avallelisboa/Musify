b712eb35b0b2438dd14305ede5fa73ebee058a40
Andrés Valle <andres.valle@wundermanmvd.local>
2020-08-28 13:08:28 -0300

    getartists implemented

--- backend/controllers/ArtistController.js
+++ backend/controllers/ArtistController.js
@@ -1,11 +1,77 @@
 'use strict'
 
+const path = require('path');
+const fs = require('fs');
+
 const express = require('express');
 const api = express.Router();
-const bcrypt = require('bcrypt-nodejs');
+const mongoosePaginate = require('mongoose-pagination');
+
+const Auth = require('../middlewares/authenticate');
 
 const Artist = require('../models/Artist');
+const Album = require('../models/Album');
+const Song = require('../models/Song');
 
 
+api.get('/artist/:artist-id', Auth.verifyAuth, (req, res)=>{
+    let artistId = req.params.id;
+
+    Artist.findById(artistId)
+            .then((artistFounded)=>{
+                res.status(200).send(json({
+                    artist: artistFounded
+                }));
+            })
+            .catch((error)=>{
+                res.status(404).send(json({
+                    message: "The artist was not founded"
+                }));
+            });
+});
+
+api.get('/artists', Auth.verifyAuth, (req, res) =>{
+    let page = req.params.page;
+    let itemsPerPage = 3;
+
+    Artist.find().sort('name').paginate(page, itemsPerPage)
+                                .then((artists,total)=>{
+                                    res.status(200).send(json({
+                                        artists: artists,
+                                        pages: total
+                                    }));
+                                })
+                                .catch((error)=>{
+                                    res.status(500).send(json({
+                                        message: "There was a problem with the pagination"
+                                    }));
+                                });
+});
+
+api.post('/new-artist', Auth.verifyAuth, (req, res)=>{
+    let params = req.body;
+
+    if(!params) res.status(200).send(json({
+        message: "You need to specify the parameters to register a new Artist"
+    }));
+
+    let artist = new Artist();
+
+    artist.name = params.name;
+    artist.description = params.description;
+    artist.image = null;
+
+    artist.save()
+            .then((artistStored)=>{
+                res.status(200).send(json({
+                    artist: artistStored,
+                }));
+            })
+            .catch((error)=>{
+                res.status(500).send(json({
+                    message: "There was a problem at saving the artist"
+                }));
+            });
+});
 
 module.exports = api;
--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,12 +1,20 @@
 'use strict'
 
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const api = express.Router();
+
 const bcrypt = require('bcrypt-nodejs');
 const jwt = require('../services/jwt');
 const Auth = require('../middlewares/authenticate');
 
+const multiparty = require('connect-multiparty');
+const upload = multiparty({uploadDir: './uploads/users'});
+
 const User = require('../models/User');
+const { json } = require('body-parser');
 
 
 api.post('/register', (req, res)=>{
@@ -82,5 +90,73 @@ api.post('/login', Auth.verifyAuth, (req,res)=>{
     });
 });
 
+api.put('/update-user/', Auth.verifyAuth, (req, res)=>{
+    let userId = req.sub;
+    let update = req.body;
+
+    if(!userId) res.status(200).send(json({
+        message: "You must enter the userId"
+    }));
+
+    User.findByIdAndUpdate(userId,update,(error, userUpdated)=>{
+        if(error){
+            res.status(500).send(json({
+                message: "There was an error while updating the user"
+            }));
+        }else if(!userUpdated){
+            res.status(404).send(json({
+                message: "The user could not be updated"
+            }));
+        }else res.status(200).send(json({
+            user: userUpdated
+        }));
+    });
+});
+
+api.post('/upload-image', [Auth.verifyAuth, upload], (req, res)=>{
+    let userId = req.params.id;
+
+    if(req.file){
+        let filePath = req.files.image.path;
+        let fileSplit = filePath.split('\\');
+        let fileName = file.Split[2];
+
+        let extSplit = fileName.split('\.');
+        let ext = extSplit[1];
+
+        if(fileExt == "png" || fileExt == "jpg" || fileExt == "jpeg" || fileExt == "gif"){
+            User.findByIdAndUpdate(userId, {image: fileName},(error, userUpdated)=>{
+                if(error){
+                    res.status(500).send(json({
+                        message: "There was an error while updating the user"
+                    }));
+                }else if(!userUpdated){
+                    res.status(404).send(json({
+                        message: "The user could not be updated"
+                    }));
+                }else res.status(200).send(json({
+                    user: userUpdated
+                }));
+            });
+        }
+    }else{
+        res.status(200).send(json({
+            message: "You have not uploaded any file"
+        }));
+    }
+});
+
+api.get('/get-image/:image-file', Auth.verifyAuth,(req, res)=>{
+    let imageFile = req.params.imageFile;
+    let pathFile = './uploads/users/' + imageFile;
+
+    fs.exists(pathFile, (exists)=>{
+        if(exists){
+            res.sendFile(path.resolve(pathFile));
+        }else res.status(404).send(json({
+            message: "The image was not founded"
+        }));
+    });
+});
 
 module.exports = api;
